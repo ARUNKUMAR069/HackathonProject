b03a39e114bd9b231c2e4102a783e235260dbac6
ARUNKUMAR069 <161587278+ARUNKUMAR069@users.noreply.github.com>
2024-08-22 22:08:07 +0530

    Give a Routing to the Pages with the help of react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,32 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
 import Login from './components/Login'
+import Signup from './components/Signup'
+import Landing from './components/Landing'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <> <Navbar /> <Landing /></>
+    },
+
+    {
+      path: "/login",
+      element: <> <Navbar /> <Login /></>
+    },
+    {
+      path: "/register",
+      element: <> <Navbar /> <Signup /></>
+    },
+  ])
 
 
   return (
-    <div className='bg-gradient-to-r from-[#000] to-slate-700 h-full w-screen  overflow-x-hidden'>
-<Navbar/>
-<Login/>
+    <div className='bg-gradient-to-r from-[#000] to-slate-700 h-full w-screen '>
+      <RouterProvider router={router} />
     </div>
-    
+
   )
 }
 
