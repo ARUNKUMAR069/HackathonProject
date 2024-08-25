8f98297efeb5bb76f90f44aefd1239b276c0e7df
ARUNKUMAR069 <161587278+ARUNKUMAR069@users.noreply.github.com>
2024-08-25 21:46:57 +0530

    Update and rename src/components/Landing.jsx to Home.jsx

---
+++ Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+
+
+const Home = () => {
+  return (
+    <div className='w-screen h-[100vh]  '> 
+
+
+    </div>
+  )
+}
+
+export default Home
--- src/components/Landing.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Landing = () => {
-  return (
-    <div className='w-screen h-[87.7vh] '> </div>
-  )
-}
-
-export default Landing
