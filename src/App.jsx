1e8ebc2e88b339dfeee979bd8a1d893a77c0d5e4
ARUNKUMAR069 <161587278+ARUNKUMAR069@users.noreply.github.com>
2024-08-25 21:43:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,31 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import Navbar from './components/Navbar'
 import Login from './components/Login'
 import Signup from './components/Signup'
-import Landing from './components/Landing'
+import Home from './components/Home'
+import { SidebarComponent } from './components/SidebarComponent'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <> <Navbar /> <Landing /></>
+      element: <> <Navbar />   <div className='flex '>  <SidebarComponent />  <Home /></div>      </>
     },
 
     {
       path: "/login",
-      element: <> <Navbar /> <Login /></>
+      element: <> <Navbar />    <div className='flex'>  <SidebarComponent />  <Login /></div>                 </>
     },
     {
       path: "/register",
-      element: <> <Navbar /> <Signup /></>
+      element: <> <Navbar />       <Signup />  </>
     },
   ])
 
 
   return (
-    <div className='bg-gradient-to-r from-[#000] to-slate-700 h-full w-screen '>
+    <div className='bg-gradient-to-r from-[#000] to-slate-700 h-auto w-screen '>
       <RouterProvider router={router} />
     </div>
 
