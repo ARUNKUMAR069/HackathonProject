d9df0799b909703928fcd7db1466f415be3f23f1
ARUNKUMAR069 <161587278+ARUNKUMAR069@users.noreply.github.com>
2024-08-25 21:42:14 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ThemeProvider } from "./components/Theme-provider.jsx";
 
 createRoot(document.getElementById('root')).render(
-
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
     <App />
-
+  </ThemeProvider>
 )
