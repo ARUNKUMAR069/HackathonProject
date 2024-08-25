537b85d7f4f79ef8f307bb08f13f0fc939921926
ARUNKUMAR069 <161587278+ARUNKUMAR069@users.noreply.github.com>
2024-08-25 21:33:51 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+
+import { keepTheme } from "keep-react/keepTheme";
+
+const config = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {},
 }
+
+export default keepTheme(config);
