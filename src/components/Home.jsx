7a11333750ec125115c669c1ceac5b340f67149d
ARUNKUMAR069 <161587278+ARUNKUMAR069@users.noreply.github.com>
2024-08-29 21:43:48 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,11 +6,15 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='w-screen h-[100vh]  '> 
-
+    <div className='w-screen h-[100vh] flex items-center  ml-4  '> 
 
     </div>
   )
 }
 
 export default Home
+
+
+
+// ignore
+{/* <div className="main-headline  flex flex-col text-white text-center justify-center items-center  p-3 font-bold font-[Inter] text-[3.5rem]">Empowering Education<span >Enabling Excellence</span> </div> */}
